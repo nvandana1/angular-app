17226db7c599e60902d39badc2332428177af5eb
Vandana Nayak <nvandana1@gmail.com>
2023-08-22 10:39:54 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+submit() {
+throw new Error('Method not implemented.');
+}
   title = 'angular-app';
 }
